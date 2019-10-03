3dbf62f9ffbd201ca33aac3ad90cd09aaf032f71
keybinding <rjewsky10@gmail.com>
2019-10-04 02:17:23 +0300

    a

---
+++ DependencyInjection/StringEvaluationExtension.php
@@ -0,0 +1,13 @@
+<?php
+namespace Arz\StringEvaluationBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class StringEvaluationExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        // ... you'll load the files here later
+    }
+}
---
+++ StringEvaluationBundle.php
@@ -0,0 +1,8 @@
+<?php
+namespace Arz\StringEvaluationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class StringEvaluationBundle extends Bundle {
+	
+}
