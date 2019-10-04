27846eb58954540283bc8fe5731f3f8fba24db58
keybinding <rjewsky10@gmail.com>
2019-10-04 08:29:10 +0300

    a

--- DependencyInjection/StringEvaluationExtension.php
+++ DependencyInjection/StringEvaluationExtension.php
@@ -1,14 +1,16 @@
 <?php
 namespace Arz\StringEvaluationBundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
 
 class StringEvaluationExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader(
+        $loader = new Loader\XmlFileLoader(
 			$container,
 			new FileLocator(__DIR__.'/../Resources/config')
 		);
